b0d22ace4f2c8d33160b4cb39941fd9ece2e0fb6
Ben Woosley <ben.woosley@gmail.com>
2020-04-11 16:29:43 -0700

    Enable depends on C++17 Travis build

--- ci/test/00_setup_env_native_cxx17.sh
+++ ci/test/00_setup_env_native_cxx17.sh
@@ -8,6 +8,5 @@ export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_native_cxx17
 export PACKAGES="python3-zmq libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-test-dev libboost-thread-dev libdb5.3++-dev libminiupnpc-dev libzmq3-dev"
-export NO_DEPENDS=1
 export GOAL="install"
 export BITCOIN_CONFIG="--enable-zmq --with-incompatible-bdb --enable-c++17"
