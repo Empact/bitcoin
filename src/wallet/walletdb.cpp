fffd5f3a0e0a30e006d5a04b1872d5256818bea0
Andrew Chow <achow101-github@achow101.com>
2020-05-08 22:45:43 -0400

    walletdb: Move PeriodicFlush into WalletDatabase
    Make PeriodicFlush a non-static member of WalletDatabase instead of
    WalletBatch.

--- src/wallet/walletdb.cpp
+++ src/wallet/walletdb.cpp
@@ -879,7 +879,7 @@ void MaybeCompactWalletDB()
         }
 
         if (dbh.nLastFlushed != nUpdateCounter && GetTime() - dbh.nLastWalletUpdate >= 2) {
-            if (BerkeleyBatch::PeriodicFlush(dbh)) {
+            if (dbh.PeriodicFlush()) {
                 dbh.nLastFlushed = nUpdateCounter;
             }
         }
