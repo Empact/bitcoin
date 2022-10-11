7b2d0b0811b0faf7f09a119354a22ecfd869720b
fanquake <fanquake@gmail.com>
2022-10-11 15:33:22 +0800

    util: move threadinterrupt into util

--- ci/test/06_script_b.sh
+++ ci/test/06_script_b.sh
@@ -54,7 +54,6 @@ if [ "${RUN_TIDY}" = "true" ]; then
           " src/rpc/fees.cpp"\
           " src/rpc/signmessage.cpp"\
           " src/test/fuzz/txorphan.cpp"\
-          " src/threadinterrupt.cpp"\
           " src/util/bip32.cpp"\
           " src/util/bytevectorhash.cpp"\
           " src/util/error.cpp"\
@@ -67,6 +66,7 @@ if [ "${RUN_TIDY}" = "true" ]; then
           " src/util/strencodings.cpp"\
           " src/util/string.cpp"\
           " src/util/syserror.cpp"\
+          " src/util/threadinterrupt.cpp"\
           " src/util/url.cpp"\
           " src/zmq"\
           " -p . ${MAKEJOBS} -- -Xiwyu --cxx17ns -Xiwyu --mapping_file=${BASE_BUILD_DIR}/bitcoin-$HOST/contrib/devtools/iwyu/bitcoin.core.imp"
